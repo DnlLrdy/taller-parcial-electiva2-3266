b9f9dafb0ea723ad5459e48ccd4abf6045ae76d2
DnlLrdy <lndplfydrlcrg@gmail.com>
2025-11-12 14:13:25 -0500

    Subiendo base del taller-parcial a GitHub

---
+++ src/main/java/com/taller/parcial/TallerParcialApplication.java
@@ -0,0 +1,13 @@
+package com.taller.parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerParcialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taller/parcial/TallerParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerParcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
